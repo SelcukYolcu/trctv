5c5ef28c8f5bdb8170cf3102a083a497c52233ba
Selcuk Yolcu <yolcus@turcom.com.tr>
2019-04-26 12:09:47 +0300

    textures and pvr.hts settings are created

---
+++ setup.sh
@@ -0,0 +1,7 @@
+echo "Kodi installation is starting ..."
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:team-xbmc/ppa
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install kodi
+sudo apt-get install kodi-pvr-tvheadend-hts
